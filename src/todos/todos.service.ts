74001570adbc5aebd9586e799d950533655dbe3b
wonmin-jeon <wonmin-jeon@wonmin-jeonui-iMac.local>
2020-03-22 22:23:42 +0900

    feature: creating a todo (part 1: service)

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Todo } from './todo.model';
+import * as uuid from 'uuid/v1';
 
 @Injectable()
 export class TodosService {
@@ -7,5 +8,17 @@ export class TodosService {
 
   getAllTodos(): Todo[] {
     return this.todos;
+  }
+
+  createTodo(title: string, description: string): Todo {
+    const todo: Todo = {
+      id: uuid(),
+      title,
+      description,
+      completed: false,
+    };
+
+    this.todos.push(todo);
+    return todo;
   }
 }
